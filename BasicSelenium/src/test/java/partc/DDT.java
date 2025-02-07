65196ebbd09265575afcbd81b1c320be03430307
viraj <viraj@viraj>
2025-02-07 20:51:14 +0530

    1

--- BasicSelenium/src/test/java/partc/DDT.java
+++ BasicSelenium/src/test/java/partc/DDT.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DDT {
+	
+	
+	//virajnaik
 public static void main(String[] args) throws Throwable {
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
